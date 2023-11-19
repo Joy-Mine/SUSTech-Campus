7c1f43519939c545b43f46a1f5dd55593e846fd1
LTTMG <LTTMG@outlook.com>
2023-11-19 21:02:45 +0800

    remove unused imports

--- src/main/java/com/sustech/campus/entity/Route.java
+++ src/main/java/com/sustech/campus/entity/Route.java
@@ -1,7 +1,5 @@
 package com.sustech.campus.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 @TableName("route")
--- src/main/java/com/sustech/campus/mapper/UserMapper.java
+++ src/main/java/com/sustech/campus/mapper/UserMapper.java
@@ -3,9 +3,6 @@ package com.sustech.campus.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.sustech.campus.entity.User;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
