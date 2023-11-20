1d50b7e99b83819587aaccaceb0dc217eb07d0f9
LTTMG <LTTMG@outlook.com>
2023-11-20 17:19:03 +0800

    refactor building service

--- src/main/java/com/sustech/campus/service/BuildingService.java
+++ src/main/java/com/sustech/campus/service/BuildingService.java
@@ -1,6 +1,7 @@
 package com.sustech.campus.service;
 
 import com.sustech.campus.entity.Building;
+import com.sustech.campus.entity.BuildingPhoto;
 
 import java.util.List;
 
@@ -60,4 +61,35 @@ public interface BuildingService {
      * @return false if failed to change the details, otherwise true
      */
     boolean changeBuildingDetails(String name, String details);
+
+    /**
+     * @param buildingName name of the building
+     * @return null if the building does not exist, otherwise, a List that contains all photos of the building
+     */
+    List<BuildingPhoto> listBuildingPhotos(String buildingName);
+
+    /**
+     * @param id
+     * @return null of the photo does not exist, otherwise, a photo instance
+     */
+    BuildingPhoto getBuildingPhotoById(Long id);
+
+    /**
+     * @param buildingName name of the building that the photo belongs to
+     * @param path path of the photo
+     * @return null if failed to add such photo, otherwise the id of the photo
+     */
+    Long addBuildingPhoto(String buildingName, String path);
+
+    /**
+     * @param id id of the photo
+     * @return false if failed to delete the photo, otherwise true
+     */
+    boolean deleteBuildingPhoto(Long id);
+
+    /**
+     * @param id id of the photo
+     * @return whether the photo with the given id exists
+     */
+    boolean buildingPhotoExists(Long id);
 }
--- src/test/java/com/sustech/campus/servicetest/BuildingServiceTest.java
+++ src/test/java/com/sustech/campus/servicetest/BuildingServiceTest.java
@@ -1,6 +1,7 @@
 package com.sustech.campus.servicetest;
 
 import com.sustech.campus.entity.Building;
+import com.sustech.campus.entity.BuildingPhoto;
 import com.sustech.campus.service.BuildingService;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,178 +18,38 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class BuildingServiceTest {
     private final BuildingService buildingService;
-    private final Building building;
+
+    private List<Building> buildings;
+
+    private List<BuildingPhoto> buildingPhotos;
 
     @Autowired
     public BuildingServiceTest(BuildingService buildingService) {
         this.buildingService = buildingService;
-        this.building = new Building();
-        this.building.setName("test_building");
-        this.building.setDescription("this is description");
-        this.building.setDetails("this is details");
-        this.building.setLatitude(1);
-        this.building.setLatitude(2);
+        int numBuildings = 4;
+        int numBuildingPhotos = 10;
+        this.buildings = new ArrayList<>();
+        this.buildingPhotos = new ArrayList<>();
+        for (int i = 0; i < numBuildings; ++i) {
+            Building building = new Building();
+            building.setName("test_building_" + i);
+            building.setDetails("");
+            building.setDescription("");
+            building.setLatitude(0);
+            building.setLongitude(0);
+            this.buildings.add(building);
+        }
+        for (int i = 0; i <numBuildingPhotos; ++i) {
+            BuildingPhoto photo = new BuildingPhoto();
+            photo.setBuilding("test_building_" + new Random().nextInt(numBuildings));
+            photo.setPath("/foo/bar/" + i);
+            this.buildingPhotos.add(photo);
+        }
     }
 
     @BeforeEach
-    void clean() {
-        this.buildingService.deleteBuilding(this.building.getName());
-    }
-
-    @Test
-    @Order(1)
-    void test() {
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-        assertNull(this.buildingService.getBuildingByName(this.building.getName()));
-
-        assertTrue(this.buildingService.addBuilding(
-                this.building.getName(),
-                this.building.getDescription(),
-                this.building.getDetails(),
-                this.building.getLatitude(),
-                this.building.getLongitude()
-        ));
-        assertNotNull(this.buildingService.getBuildingByName(this.building.getName()));
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getName(),
-                this.building.getName()
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDescription(),
-                this.building.getDescription()
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDetails(),
-                this.building.getDetails()
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLatitude(),
-                this.building.getLatitude()
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLongitude(),
-                this.building.getLongitude()
-        );
-
-        assertTrue(this.buildingService.deleteBuilding(this.building.getName()));
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-        assertNull(this.buildingService.getBuildingByName(this.building.getName()));
-    }
-
-    @Test
-    @Order(2)
-    void testChangeBuildingDescription() {
-        String newDescription = "this is the new description";
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-        assertTrue(this.buildingService.addBuilding(
-                this.building.getName(),
-                this.building.getDescription(),
-                this.building.getDetails(),
-                this.building.getLatitude(),
-                this.building.getLongitude()
-        ));
-
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDescription(),
-                this.building.getDescription()
-        );
-        assertTrue(this.buildingService.changeBuildingDescription(
-                this.building.getName(),
-                newDescription
-        ));
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDescription(),
-                newDescription
-        );
-
-        assertTrue(this.buildingService.deleteBuilding(this.building.getName()));
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-    }
-
-    @Test
-    @Order(3)
-    void testChangeBuildingDetails() {
-        String newDetails = "this is the new details";
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-        assertTrue(this.buildingService.addBuilding(
-                this.building.getName(),
-                this.building.getDescription(),
-                this.building.getDetails(),
-                this.building.getLatitude(),
-                this.building.getLongitude()
-        ));
-
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDetails(),
-                this.building.getDetails()
-        );
-        assertTrue(this.buildingService.changeBuildingDetails(
-                this.building.getName(),
-                newDetails
-        ));
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getDetails(),
-                newDetails
-        );
-
-        assertTrue(this.buildingService.deleteBuilding(this.building.getName()));
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-    }
-
-    @Test
-    @Order(4)
-    void testChangeBuildingLocation() {
-        double newLatitude = 3, newLongitude = 4;
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-        assertTrue(this.buildingService.addBuilding(
-                this.building.getName(),
-                this.building.getDescription(),
-                this.building.getDetails(),
-                this.building.getLatitude(),
-                this.building.getLongitude()
-        ));
-
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLatitude(),
-                this.building.getLatitude()
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLongitude(),
-                this.building.getLongitude()
-        );
-        assertTrue(this.buildingService.changeBuildingLocation(
-                this.building.getName(),
-                newLatitude, newLongitude
-        ));
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLatitude(),
-                newLatitude
-        );
-        assertEquals(
-                this.buildingService.getBuildingByName(this.building.getName()).getLongitude(),
-                newLongitude
-        );
-
-        assertTrue(this.buildingService.deleteBuilding(this.building.getName()));
-        assertFalse(this.buildingService.buildingExists(this.building.getName()));
-    }
-
-    @Test
-    @Order(5)
-    void testListAllBuildings() {
-        List<Building> buildings = new ArrayList<>();
-        for (int i = 0; i < 10; ++i) {
-            Building building = new Building();
-            building.setName("test_building_" + i);
-            building.setDescription("description_" + i);
-            building.setDetails("details_" + i);
-            building.setLatitude(-i);
-            building.setLongitude(i);
-            buildings.add(building);
-        }
-
-        for (Building building : buildings) {
-            assertFalse(this.buildingService.buildingExists(building.getName()));
+    void insert() {
+        for (Building building : this.buildings) {
             assertTrue(this.buildingService.addBuilding(
                     building.getName(),
                     building.getDescription(),
@@ -196,16 +58,136 @@ public class BuildingServiceTest {
                     building.getLongitude()
             ));
         }
+        for (BuildingPhoto buildingPhoto : this.buildingPhotos) {
+            Long result = this.buildingService.addBuildingPhoto(
+                    buildingPhoto.getBuilding(),
+                    buildingPhoto.getPath()
+            );
+            assertNotNull(result);
+            buildingPhoto.setId(result);
+        }
+    }
 
+    @AfterEach
+    void clean() {
+        for (BuildingPhoto photo : this.buildingPhotos) {
+            assertTrue(this.buildingService.deleteBuildingPhoto(photo.getId()));
+        }
+        for (Building building : this.buildings) {
+            assertTrue(this.buildingService.deleteBuilding(building.getName()));
+        }
+    }
+    
+    @Test
+    @Order(1)
+    void testListAllBuildings() {
         assertIterableEquals(
                 this.buildingService.listAllBuildings().stream()
                         .sorted(Comparator.comparing(Building::getName)).toList(),
-                buildings.stream().sorted(Comparator.comparing(Building::getName)).toList()
+                this.buildings.stream().sorted(Comparator.comparing(Building::getName)).toList()
         );
-
-        for (Building building : buildings) {
-            assertTrue(this.buildingService.deleteBuilding(building.getName()));
-            assertFalse(this.buildingService.buildingExists(building.getName()));
+    }
+    
+    @Test
+    @Order(2)
+    void testGetBuildingByName() {
+        for (Building building : this.buildings) {
+            assertEquals(
+                    this.buildingService.getBuildingByName(building.getName()),
+                    building
+            );
         }
     }
+    
+    @Test
+    @Order(3)
+    void testBuildingExists() {
+        for (Building building : this.buildings) {
+            assertTrue(this.buildingService.buildingExists(building.getName()));
+        }
+    }
+    
+    @Test
+    @Order(4)
+    void testChangeBuildingLocation() {
+        for (Building building : this.buildings) {
+            assertTrue(this.buildingService.changeBuildingLocation(
+                    building.getName(),
+                    building.getLatitude() + 1,
+                    building.getLongitude() + 1
+            ));
+            building.setLongitude(building.getLongitude() + 1);
+            building.setLatitude(building.getLatitude() + 1);
+            assertEquals(
+                    this.buildingService.getBuildingByName(building.getName()),
+                    building
+            );
+        }
+    }
+
+    @Test
+    @Order(5)
+    void testChangeBuildingDescription() {
+        for (Building building : this.buildings) {
+            assertTrue(this.buildingService.changeBuildingDescription(
+                    building.getName(),
+                    "new description"
+            ));
+            building.setDescription("new description");
+            assertEquals(
+                    this.buildingService.getBuildingByName(building.getName()),
+                    building
+            );
+        }
+    }
+
+    @Test
+    @Order(6)
+    void testChangeBuildingDetails() {
+        for (Building building : this.buildings) {
+            assertTrue(this.buildingService.changeBuildingDetails(
+                    building.getName(),
+                    "new details"
+            ));
+            building.setDetails("new details");
+            assertEquals(
+                    this.buildingService.getBuildingByName(building.getName()),
+                    building
+            );
+        }
+    }
+
+    @Test
+    @Order(7)
+    void testBuildingPhotoExists() {
+        for (BuildingPhoto photo : this.buildingPhotos) {
+            assertTrue(this.buildingService.buildingPhotoExists(photo.getId()));
+        }
+    }
+
+    @Test
+    @Order(8)
+    void testGetPhotoById() {
+        for (BuildingPhoto photo : this.buildingPhotos) {
+            assertEquals(
+                    this.buildingService.getBuildingPhotoById(photo.getId()),
+                    photo
+            );
+        }
+    }
+
+    @Test
+    @Order(9)
+    void testListBuildingPhotos() {
+        for (Building building : this.buildings) {
+            assertIterableEquals(
+                    this.buildingService.listBuildingPhotos(building.getName()).stream()
+                            .sorted(Comparator.comparing(BuildingPhoto::getId)).toList(),
+                    this.buildingPhotos.stream().filter(e -> e.getBuilding().equals(building.getName()))
+                            .sorted(Comparator.comparing(BuildingPhoto::getId)).toList()
+            );
+        }
+    }
+    
+    
 }
