c110178ea8bc5a69e92b3b71a92da9e25b043eae
Joy-Mine <1525323329@qq.com>
2023-12-27 19:36:11 +0800

    增加获取商店图片url

--- src/main/java/com/sustech/campus/controller/StoreController.java
+++ src/main/java/com/sustech/campus/controller/StoreController.java
@@ -1,6 +1,7 @@
 package com.sustech.campus.controller;
 
 import com.sustech.campus.entity.Goods;
+import com.sustech.campus.entity.GoodsPhoto;
 import com.sustech.campus.entity.Store;
 import com.sustech.campus.enums.UserType;
 import com.sustech.campus.interceptor.Access;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @RestController
@@ -84,4 +86,60 @@ public class StoreController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Goods not found.");
         }
     }
+
+    @Access(level = UserType.ADMIN)
+    @PatchMapping("/goods/change-price/{goodsId}")
+    public ResponseEntity<String> changeGoodsPrice(@PathVariable Long goodsId, @RequestParam BigDecimal newPrice) {
+        boolean success = storeService.changeGoodsPrice(goodsId, newPrice);
+        if (success) {
+            return ResponseEntity.ok("Goods price updated successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Goods not found.");
+        }
+    }
+
+    @Access(level = UserType.ADMIN)
+    @PatchMapping("/goods/change-quantity/{goodsId}")
+    public ResponseEntity<String> changeGoodsQuantity(@PathVariable Long goodsId, @RequestParam Integer newQuantity) {
+        boolean success = storeService.changeGoodsQuantity(goodsId, newQuantity);
+        if (success) {
+            return ResponseEntity.ok("Goods quantity updated successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Goods not found.");
+        }
+    }
+
+    @Access(level = UserType.ADMIN)
+    @PostMapping("/goods/photo/add")
+    public ResponseEntity<String> addGoodsPhoto(@RequestParam Long goodsId, @RequestParam String photoPath) {
+        Long photoId = storeService.addGoodsPhoto(goodsId, photoPath);
+        if (photoId != null) {
+            return ResponseEntity.ok("Goods photo added successfully. ID: " + photoId);
+        } else {
+            return ResponseEntity.badRequest().body("Error adding goods photo.");
+        }
+    }
+
+    @Access(level = UserType.ADMIN)
+    @GetMapping("/goods/photo/{goodsId}")
+    public ResponseEntity<List<GoodsPhoto>> listAllGoodsPhotos(@PathVariable Long goodsId) {
+        List<GoodsPhoto> photos = storeService.listAllGoodsPhotos(goodsId);
+        if (photos != null && !photos.isEmpty()) {
+            return ResponseEntity.ok(photos);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @Access(level = UserType.ADMIN)
+    @DeleteMapping("/goods/photo/delete/{photoId}")
+    public ResponseEntity<String> deleteGoodsPhoto(@PathVariable Long photoId) {
+        boolean success = storeService.deleteGoodsPhoto(photoId);
+        if (success) {
+            return ResponseEntity.ok("Goods photo deleted successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Goods photo not found.");
+        }
+    }
+
 }
