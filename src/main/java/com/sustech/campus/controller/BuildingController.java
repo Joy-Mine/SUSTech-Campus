478facb09414645dda336720f06fd6572f262209
SaxonNB <your email>
2023-12-30 00:07:42 +0800

    add different port

--- src/main/java/com/sustech/campus/controller/BuildingController.java
+++ src/main/java/com/sustech/campus/controller/BuildingController.java
@@ -152,7 +152,7 @@ public class BuildingController {
     @GetMapping("/{id}")
     public ResponseEntity<Building> getBuildingByName(@PathVariable String id) {
         Building building = buildingService.getBuildingById(Long.valueOf(id));
-        building.setCoverPath("localhost:8082/building/image/"+buildingService.listBuildingPhotos((building.getId())).get(0).getPath());
+        building.setCoverPath("http://localhost:8082/building/image/"+buildingService.listBuildingPhotos((building.getId())).get(0).getPath());
         if (building != null) {
             return ResponseEntity.ok(building);
         } else {
